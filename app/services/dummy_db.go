89c21222661a6476c33d8952e8fe1c191f9054ae
razanlrahardjo <razanlrahardjo@gmail.com>
2021-10-04 00:27:30 +0700

    second commit

--- app/services/dummy_db.go
+++ app/services/dummy_db.go
@@ -1,27 +1,27 @@
 package services
 
-import (
-	"github.com/razanlrahardjo/hacktiv8/app/migrations"
+// import (
+// 	"github.com/razanlrahardjo/hacktiv8/app/migrations"
 
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-)
+// 	"gorm.io/driver/sqlite"
+// 	"gorm.io/gorm"
+// 	"gorm.io/gorm/logger"
+// )
 
-// DBConnectTest test
-func DBConnectTest() *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
-	})
-	if nil != err {
-		panic(err)
-	}
+// // DBConnectTest test
+// func DBConnectTest() *gorm.DB {
+// 	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{
+// 		Logger: logger.Default.LogMode(logger.Silent),
+// 	})
+// 	if nil != err {
+// 		panic(err)
+// 	}
 
-	err = db.AutoMigrate(migrations.ModelMigrations...)
-	if nil != err {
-		panic(err)
-	}
-	DB = db
+// 	err = db.AutoMigrate(migrations.ModelMigrations...)
+// 	if nil != err {
+// 		panic(err)
+// 	}
+// 	DB = db
 
-	return db
-}
+// 	return db
+// }
